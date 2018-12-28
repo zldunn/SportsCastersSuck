bb13c6a4f11ed54c1db47e6f95c69ba66059483e
Zane Dunnings <zane@Zanes-MBP.hsd1.mi.comcast.net>
2018-12-28 13:09:53 -0500

    reorganized file structure and props and started on the details section

---
+++ web/src/components/header/CommentatorDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './../styleSheet/CommentatorDetails.css';
+
+class CommentatorDetails extends Component {
+
+  render() {
+    return (
+      <div className="Details" />
+    );
+  }
+}
+
+export default CommentatorDetails;
